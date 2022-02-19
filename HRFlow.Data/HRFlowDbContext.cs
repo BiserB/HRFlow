27acfff14e7c2631e5117c3f18996b9e3ce0393d
BiserB <butoff@abv.bg>
2022-02-19 18:14:20 +0200

    Initial commit

---
+++ HRFlow.Data/HRFlowDbContext.cs
@@ -0,0 +1,20 @@
+﻿using HRFlow.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRFlow.Data
+{
+    public class HRFlowDbContext : DbContext
+    {
+        public HRFlowDbContext(DbContextOptions<HRFlowDbContext> options): base(options)
+        {
+
+        }
+
+        DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseSerialColumns();
+        }
+    }
+}
